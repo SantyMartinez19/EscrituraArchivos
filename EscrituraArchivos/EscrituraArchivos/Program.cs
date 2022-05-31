4af50176626b68f56cc0dbf5f4003af931d9d022
SantyMartinez19 <105743090+SantyMartinez19@users.noreply.github.com>
2022-05-31 11:26:45 -0700

    Programa - Version 1

---
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // <-- libreria para subir los archivos
+
+namespace EscrituraArchivos
+{
+    //Nombre: Santy Francisco Martinez Castellanos
+    //N.Control: 21211989
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            // si el archivo no existe -- lo creara
+            // si ya existe, escribira en el
+            //true es para agregar y no sobreescribir
+
+            string[] lines = {
+                "Esta es la informacion de la primera linea",
+                "Esta es la segunda linea",
+                "Fin del texto"
+            };
+
+            //Recorre el arreglo
+            foreach(string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+
+            sw.Close();
+
+            Console.WriteLine("Escribiendo en el archivo...");
+          
+            
+           
+        }
+    }
+}
