f452a489d5b1256fa786757c693b1f77d3af8838
SantyMartinez19 <105743090+SantyMartinez19@users.noreply.github.com>
2022-05-31 11:49:43 -0700

    Programa - Version 2 - Nombres

---
+++ EscrituraArchivos - 2/EscrituraArchivos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // <-- libreria para subir los archivos
+
+namespace EscrituraArchivos
+{
+    //Nombre: Santy Francisco Martinez Castellanos
+    //N.Control: 21211989
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingresa un nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Presione enter");
+            Console.ReadKey();
+
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            // si el archivo no existe -- lo creara
+            // si ya existe, escribira en el
+            //true es para agregar y no sobreescribir
+
+            string[] lines = {
+                nombre
+                
+            };
+
+            //Recorre el arreglo
+            foreach(string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+
+            sw.Close();
+
+            Console.WriteLine("Escribiendo en el archivo...");
+            
+            
+                
+        }
+    }
+}
